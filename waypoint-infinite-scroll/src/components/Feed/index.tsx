332cd0eff6fde3656cf0268eeb4d830eb41f820b
Pewww <jung22337@naver.com>
2019-06-30 01:36:18 +0900

    :pencil: IFeedItem 인터페이스 FeedItem 컴포넌트와 같이 import되도록 구문 수정

--- waypoint-infinite-scroll/src/components/Feed/index.tsx
+++ waypoint-infinite-scroll/src/components/Feed/index.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 import InfiniteScroll from '../InfiniteScroll';
 import Loading from '../Loading';
 import axios from 'axios';
-import {IFeedItem} from './FeedItem';
-import FeedItem from './FeedItem';
+import FeedItem, {IFeedItem} from './FeedItem';
 import styled from 'styled-components';
 
 interface Props {
