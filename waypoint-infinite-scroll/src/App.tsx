e482606f564915659940a038fe0ac5412558f949
Pewww <jung22337@naver.com>
2019-06-30 01:24:50 +0900

    :sparkles: waypoint-infinite-scroll 및 간단한 피드 구현 완료

--- waypoint-infinite-scroll/src/App.tsx
+++ waypoint-infinite-scroll/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Feed from './components/Feed';
-import './App.css';
 
 const BASE_URL = 'http://localhost:5257/feed/';
 
--- waypoint-infinite-scroll/src/index.tsx
+++ waypoint-infinite-scroll/src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <style jsx global>{`
+      html, body {
+        margin: 0;
+        height: 100%;
+      }
+      
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+          monospace;
+      }
+    `}</style>
+    <App />
+  </>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
